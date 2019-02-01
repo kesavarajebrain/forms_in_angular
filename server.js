eb907405116b5a65dbfde968b885879a789f1b17
eBrainTechnologies <ulaganathank@ebraintechnologies.com>
2019-02-01 17:13:06 +0530

    hero form

--- server.js
+++ server.js
@@ -9,28 +9,30 @@ const PORT =3000;
 app.use(bodyParser.json());
 
 app.use(cors());
-
+// Serve only the static files form the dist directory
+app.use(function(req, res, next) {
+    //set headers to allow cross origin request.
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+});
+app.use(express.static(__dirname + '/dist/angular-forms'));
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/angular-forms/index.html'));
+    });
+app.post('/postdata',function(req,res){
+        console.log(req.body);
+        res.status(200).send({"message":"Data received successfully"});
+        })    
 // app.get('/', function(req,res){
 //     res.send('Express server working fine');
 // })
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-forms'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/angular-forms/index.html'));
-});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-app.post('/postdata',function(req,res){
-console.log(req.body);
-res.status(200).send({"message":"Data received successfully"});
-})
-
-
-app.listen(PORT, function(){
-    console.log("Server running on localhost :" + PORT)
-});
+// app.listen(PORT, function(){
+//     console.log("Server running on localhost :" + PORT)
+// });
 
 
 
