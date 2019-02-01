434e8f5cc9539ade196e848518c37b81a8856a7e
eBrainTechnologies <ulaganathank@ebraintechnologies.com>
2019-02-01 16:08:53 +0530

    hero

--- src/app/server.js
+++ src/app/server.js
@@ -36,7 +36,6 @@ app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/angular-forms/index.html'));
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
